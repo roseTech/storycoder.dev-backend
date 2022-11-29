90034770a36cb8547efc24a89f669d8cd53c9a65
Thomas Stauffer <thomasstauffer@gmx.ch>
2022-11-29 22:55:55 +0100

    Add linkedom

--- examples.js
+++ examples.js
@@ -5,6 +5,7 @@ import * as process from 'process';
 import * as dotenv from 'dotenv'; // https://www.npmjs.com/package/dotenv
 import MarkdownIt from 'markdown-it'; // https://www.npmjs.com/package/markdown-it
 import YAML from 'yaml'; // https://www.npmjs.com/package/yaml
+import { DOMParser } from 'linkedom'; // https://www.npmjs.com/package/linkedom
 
 function example_dotenv() {
     dotenv.config();
@@ -28,6 +29,20 @@ function example_markdown() {
     console.log(md.render(EXAMPLE_MARKDOWN));
 }
 
+const EXAMPLE_HTML = `
+<p>a b c</p>
+<p>d e</p>
+<p>f <strong>strong</strong> g</p>
+`
+
+function example_linkedom() {
+    const document = (new DOMParser()).parseFromString(EXAMPLE_HTML, 'text/html');
+    document.querySelectorAll('p').forEach(node => {
+        console.log(node.innerText);
+    });
+}
+
 example_dotenv();
 example_yaml();
 example_markdown();
+example_linkedom();
