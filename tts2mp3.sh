eb0d869915900b270e8333d0ab5e48cdc1517d7d
Thomas Stauffer <thomasstauffer@gmx.ch>
2023-02-11 14:23:59 +0100

    Add requirements to tts2mp3

--- tts2mp3.sh
+++ tts2mp3.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
+# Debian
+#
+# pip3 install tts
+# apt install ffmpeg
+
 for DIR in *
 do
     FN_TTS="$DIR/${DIR}_Story.md.dev.tts"
     FN_WAV="$DIR/$DIR.dev.wav"
     FN_MP3="$DIR/$DIR.mp3"
-    if [ ! -f "$FN_TTS" ] # if there is no text to speak, continue
+    if [ ! -f "$FN_TTS" ] # if there is no text to speak, skip to next file
     then
         continue
     fi
-    if [ -f "$FN_MP3" ] # if the mp3 files is already existing, continue
+    if [ -f "$FN_MP3" ] # if the mp3 files is already existing, skip to next file
     then
         continue
     fi
