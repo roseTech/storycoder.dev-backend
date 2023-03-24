9acfe336d6082748d3dff6159733e6c6413acc2f
Thomas Stauffer <thomasstauffer@gmx.ch>
2023-03-24 06:22:34 +0100

    also use tortoise_tts to generate audio files

--- tts2mp3.sh
+++ tts2mp3.sh
@@ -2,25 +2,39 @@
 
 # Debian
 #
-# pip3 install tts
 # apt install ffmpeg
 
 for DIR in *
 do
+    echo "$DIR"
     FN_TTS="$DIR/${DIR}_Story.md.dev.tts"
     FN_WAV="$DIR/$DIR.dev.wav"
     FN_MP3="$DIR/$DIR.mp3"
     if [ ! -f "$FN_TTS" ] # if there is no text to speak, skip to next file
     then
+        echo "skip ..."
         continue
     fi
     if [ -f "$FN_MP3" ] # if the mp3 files is already existing, skip to next file
     then
+        echo "skip ..."
         continue
     fi
-    echo "$DIR"
+    echo "generate wav ..."
     TEXT=`cat "$FN_TTS"`
+    date
+    
+    # Coqui TTS
+    # Samples: http://erogol.com/ddc-samples/
+    # Install: pip3 install tts
     tts --text "$TEXT" --out_path "$FN_WAV"
+
+    # Tortoise TTS
+    # Samples: https://nonint.com/static/tortoise_v2_examples.html
+    # tortoise_tts.py -v angie -o "$FN_WAV" "$TEXT"
+    
+    date
+    echo "convert mp3 ..."
     ffmpeg "$FN_MP3" -i "$FN_WAV"
     rm "$FN_WAV"
 done
