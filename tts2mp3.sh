76e8f9fd143deba2a9eb3828e4709e0cfd3789f0
Thomas Stauffer <thomasstauffer@gmx.ch>
2023-04-19 18:13:33 +0200

    tts2mp3 add comments

--- tts2mp3.sh
+++ tts2mp3.sh
@@ -4,6 +4,28 @@
 #
 # apt install ffmpeg
 
+# Coqui TTS
+# Samples: http://erogol.com/ddc-samples/
+# Install: pip3 install tts
+#
+# Ca. 10 minutes for a single story. Sometimes has problems and mumbles for
+# several minutes until getting back on track. Maybe it is necessary to prepare
+# stories somewhat better and rip out everything which may lead to issue
+# (numbers, code, ...).
+
+# Tortoise TTS
+# Samples: https://nonint.com/static/tortoise_v2_examples.html
+# Install: https://github.com/neonbjb/tortoise-tts
+#
+# Installing the dependencies was not working as described (dependencies may
+# have changed since the the author updated the code). Following steps were
+# done: Use Python 3.8 (2023-04-19 not working with Python 3.10), venv was used
+# (especially good for the install step), scipy had to added manually as the
+# version described does not work anymore.
+#
+# A single story takes ca. 2 hours to encode with the ultra_fast preset. Maybe
+# someone else finds faster library which is still as good.
+
 for DIR in *
 do
     if [ ! -d "$DIR" ] # not a directory
@@ -31,16 +53,7 @@ do
     echo "generate wav ..."
     TEXT=`cat "$FN_TTS"`
     date
-    # Coqui TTS
-    # Samples: http://erogol.com/ddc-samples/
-    # Install: pip3 install tts
-    # tts --list_models
-    tts --model_name tts_models/de/thorsten/tacotron2-DCA --text "$TEXT" --out_path "$FN_WAV"
-    # tts --text "$TEXT" --out_path "$FN_WAV"
-    # Tortoise TTS
-    # Samples: https://nonint.com/static/tortoise_v2_examples.html
-    # Install: https://github.com/neonbjb/tortoise-tts
-    # Notes: use venv, scipy manually, python3.8 (2023-04-19 not working with python3.10)
+    tts --text "$TEXT" --out_path "$FN_WAV"
     #tortoise_tts.py -p ultra_fast -v angie -o "$FN_WAV" "$TEXT"
     date
 
