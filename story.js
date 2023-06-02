496d1075686c5dc79b72020d395a2a2ee060a62b
Mateusz Halada <mateusz.halada@ch.gudel.com>
2023-06-02 14:15:41 +0200

    Add eslint, prettier and fix errors according to linter

--- story.js
+++ story.js
@@ -5,35 +5,25 @@ import dotenv from 'dotenv'; // https://www.npmjs.com/package/dotenv
 import MarkdownIt from 'markdown-it'; // https://www.npmjs.com/package/markdown-it
 import process from 'process';
 import YAML from 'yaml'; // https://www.npmjs.com/package/yaml
-
-import * as functions from './functions.js';
+import * as functions from './functions';
 
 dotenv.config();
 
 const URL = process.env.WP_URL;
 
-// format('Hello {0}', 'World') -> 'Hello World'
-function formatn(text, ...vars) {
-    return text.replace(/{(\d+)}/g, function (match, number) {
-        return typeof vars[number] === 'undefined' ? match : vars[number];
-    });
-}
-
 // format('Hello {t}', {t: 'World'}) -> 'Hello World'
 function format(text, vars) {
-    return text.replace(/{(\w+)}/g, function (_, match) {
-        return typeof vars[match] === 'undefined' ? match : vars[match];
-    });
+  return text.replace(/{(\w+)}/g, (_, match) => (typeof vars[match] === 'undefined' ? match : vars[match]));
 }
 
 // split a document into the frontmatter part and the markdown part
 // https://jekyllrb.com/docs/front-matter/
 function documentSplit(text) {
-    const match = text.match(/\s*---\s*(.*?)\s*---\s*/s);
-    const frontmatterString = match[1];
-    const frontmatter = YAML.parse(frontmatterString);
-    const markdown = text.slice(match[0].length);
-    return [frontmatter, markdown];
+  const match = text.match(/\s*---\s*(.*?)\s*---\s*/s);
+  const frontmatterString = match[1];
+  const frontmatter = YAML.parse(frontmatterString);
+  const markdown = text.slice(match[0].length);
+  return [frontmatter, markdown];
 }
 
 const HTML_HEADER = `
@@ -105,89 +95,102 @@ const SOLUTION_HTML = '<input type="text"><button onclick="check(this)">Check</b
 
 // replace every <div data-solution="..."><div> with HTML
 function htmlAdjustSolution(document) {
-    document.querySelectorAll('div[data-solution]').forEach(nodeDiv => {
-        nodeDiv.setAttribute('data-solution', functions.checksum(nodeDiv.getAttribute('data-solution')));
-        nodeDiv.innerHTML = SOLUTION_HTML;
-    });
+  document.querySelectorAll('div[data-solution]').forEach((nodeDiv) => {
+    nodeDiv.setAttribute('data-solution', functions.checksum(nodeDiv.getAttribute('data-solution')));
+    nodeDiv.innerHTML = SOLUTION_HTML;
+  });
 }
 
 function htmlAdjustLinks(document, medias) {
-    document.querySelectorAll('img').forEach(node => {
-        const linkCurrent = node.getAttribute('src');
-        // TODO ideally all images are handled identically
-        if (linkCurrent.startsWith('/')) {
-            return;
-        }
-        const linkNew = '/wp-content/uploads/' + medias[linkCurrent].link;
-        node.setAttribute('src', linkNew);
-    });
+  document.querySelectorAll('img').forEach((node) => {
+    const linkCurrent = node.getAttribute('src');
+    // TODO ideally all images are handled identically
+    if (linkCurrent.startsWith('/')) {
+      return;
+    }
+    const linkNew = `/wp-content/uploads/${medias[linkCurrent].link}`;
+    node.setAttribute('src', linkNew);
+  });
 }
 
 // WordPress interprets a newline as a new block
 function htmlAdjustNewLine(document) {
-    document.querySelectorAll('p, script').forEach(node => {
-        node.innerHTML = node.innerHTML.replaceAll('\n', ' ');
-    });
+  document.querySelectorAll('p, script').forEach((node) => {
+    node.innerHTML = node.innerHTML.replaceAll('\n', ' ');
+  });
 }
 
 export function toHtml(folder, story, medias) {
-    const imageLink = medias.logoImage.link;
-    const ttsLink = medias.ttsAudio.link;
-    const options = { html: true };
-    const md = new MarkdownIt(options);
-    const [frontmatter, markdown] = documentSplit(story);
-    const linkGitHub = 'https://github.com/roseTech/storycoder.dev/tree/main/' + folder;
-    const linkGoogleTranslateStory = folder.replaceAll('_', '-').toLowerCase();
-    const linkGoogleTranslate = URL.replaceAll('.', '-') + '.translate.goog/' + linkGoogleTranslateStory + '/?_x_tr_sl=auto&_x_tr_tl=en';
-    const vars = {
-        codingLevel: frontmatter['Coding Level'],
-        codingIdeas: frontmatter['Coding Ideas'],
-        storyGenre: frontmatter['Story Genre'],
-        storyContent: frontmatter['Story Content'],
-        storyLicense: frontmatter['Story License'],
-        imageLicense: frontmatter['Image License'],
-        imageSource: frontmatter['Image Source'],
-        imageLink: imageLink,
-        ttsLink: ttsLink,
-        category: frontmatter['Category'],
-        author: frontmatter['Author'],
-        title: frontmatter.Title,
-        linkGitHub: linkGitHub,
-        linkGoogleTranslate: linkGoogleTranslate,
-    };
-    const htmlHeader = format(HTML_HEADER, vars);
-    const htmlFooter = format(HTML_FOOTER, vars);
-    const htmlMarkdown = md.render(markdown);
-    const html = SOLUTION_JS + htmlHeader + htmlMarkdown + htmlFooter;
-    const document = (new DOMParser()).parseFromString('<html>' + html + '</html>', 'text/html');
-    htmlAdjustNewLine(document);
-    htmlAdjustSolution(document);
-    htmlAdjustLinks(document, medias);
-    const htmlFixed = document.documentElement.innerHTML;
-    return htmlFixed;
+  const imageLink = medias.logoImage.link;
+  const ttsLink = medias.ttsAudio.link;
+  const options = { html: true };
+  const md = new MarkdownIt(options);
+  const [frontmatter, markdown] = documentSplit(story);
+  const linkGitHub = `https://github.com/roseTech/storycoder.dev/tree/main/${folder}`;
+  const linkGoogleTranslateStory = folder.replaceAll('_', '-').toLowerCase();
+  const linkGoogleTranslate = `${URL.replaceAll(
+    '.',
+    '-'
+  )}.translate.goog/${linkGoogleTranslateStory}/?_x_tr_sl=auto&_x_tr_tl=en`;
+  const vars = {
+    codingLevel: frontmatter['Coding Level'],
+    codingIdeas: frontmatter['Coding Ideas'],
+    storyGenre: frontmatter['Story Genre'],
+    storyContent: frontmatter['Story Content'],
+    storyLicense: frontmatter['Story License'],
+    imageLicense: frontmatter['Image License'],
+    imageSource: frontmatter['Image Source'],
+    category: frontmatter['Category'],
+    author: frontmatter['Author'],
+    title: frontmatter['Title'],
+    imageLink,
+    ttsLink,
+    linkGitHub,
+    linkGoogleTranslate
+  };
+  const htmlHeader = format(HTML_HEADER, vars);
+  const htmlFooter = format(HTML_FOOTER, vars);
+  const htmlMarkdown = md.render(markdown);
+  const html = SOLUTION_JS + htmlHeader + htmlMarkdown + htmlFooter;
+  const document = new DOMParser().parseFromString(`<html>${html}</html>`, 'text/html');
+  htmlAdjustNewLine(document);
+  htmlAdjustSolution(document);
+  htmlAdjustLinks(document, medias);
+  return document.documentElement.innerHTML;
 }
 
 export function getTags(story) {
-    const [frontmatter, _] = documentSplit(story);
+  const [frontmatter, _] = documentSplit(story);
 
-    function split(key) {
-        const value = frontmatter[key];
-        return value === null ? [] : frontmatter[key].split(',').map($ => $.trim().toLowerCase()).filter($ => $.length > 1);
-    }
-    const tags = [].concat(split('Category'), split('Story Content'), split('Story Genre'), split('Coding Level'), split('Coding Ideas'));
-    return tags;
+  function split(key) {
+    const value = frontmatter[key];
+    return value === null
+      ? []
+      : frontmatter[key]
+          .split(',')
+          .map(($) => $.trim().toLowerCase())
+          .filter(($) => $.length > 1);
+  }
+  const tags = [].concat(
+    split('Category'),
+    split('Story Content'),
+    split('Story Genre'),
+    split('Coding Level'),
+    split('Coding Ideas')
+  );
+  return tags;
 }
 
 export function toTtsText(story) {
-    const [_, markdown] = documentSplit(story);
+  const [_, markdown] = documentSplit(story);
 
-    // http://erogol.com/ddc-samples/
+  // http://erogol.com/ddc-samples/
 
-    let text = markdown;
-    text = text.replace(/<div.*?div>/g, '');
-    text = text.replace(/[^a-zA-Z0-9$!?',:%\+\-\.\n ]/g, '');
-    text = text.replaceAll('\n', ' ');
-    text = text.replace(/ +/g, ' ');
+  let text = markdown;
+  text = text.replace(/<div.*?div>/g, '');
+  text = text.replace(/[^a-zA-Z0-9$!?',:%+\-.\n ]/g, '');
+  text = text.replaceAll('\n', ' ');
+  text = text.replace(/ +/g, ' ');
 
-    return text;
+  return text;
 }
--- wp.js
+++ wp.js
@@ -8,153 +8,155 @@ import http2 from 'http2';
 
 dotenv.config();
 
-const AUTH = process.env.WP_API_USERNAME + ':' + process.env.WP_API_PASSWORD;
+const AUTH = `${process.env.WP_API_USERNAME}:${process.env.WP_API_PASSWORD}`;
 
 const URL = process.env.WP_URL;
-const URL_POSTS = URL + '/wp-json/wp/v2/posts';
-const URL_TAGS = URL + '/wp-json/wp/v2/tags';
-const URL_CATEGORIES = URL + '/wp-json/wp/v2/categories';
-const URL_MEDIA = URL + '/wp-json/wp/v2/media';
+const URL_POSTS = `${URL}/wp-json/wp/v2/posts`;
+const URL_TAGS = `${URL}/wp-json/wp/v2/tags`;
+const URL_CATEGORIES = `${URL}/wp-json/wp/v2/categories`;
+const URL_MEDIA = `${URL}/wp-json/wp/v2/media`;
 
 function checkReponse(response, body) {
-    const isOk = response.statusCode === http2.constants.HTTP_STATUS_OK;
-    const isCreated = response.statusCode === http2.constants.HTTP_STATUS_CREATED;
-    if (!(isOk || isCreated)) {
-        throw 'Invalid Response ' + response.statusCode + ' ' + JSON.stringify(body);
-    }
+  const isOk = response.statusCode === http2.constants.HTTP_STATUS_OK;
+  const isCreated = response.statusCode === http2.constants.HTTP_STATUS_CREATED;
+  if (!(isOk || isCreated)) {
+    throw Error(`Invalid Response ${response.statusCode} ${JSON.stringify(body)}`);
+  }
 }
 
 // HTTP GET
 async function get(url) {
-    console.log('get ' + url);
-    return new Promise((resolve) => {
-        let responseBody = '';
-        const options = {};
-        https.get(url, options, response => {
-            response.on('data', chunk => responseBody += chunk);
-            response.on('end', () => {
-                const body = JSON.parse(responseBody);
-                checkReponse(response, body);
-                resolve(body);
-            });
-        });
+  console.log(`get ${url}`);
+  return new Promise((resolve) => {
+    let responseBody = '';
+    const options = {};
+    https.get(url, options, (response) => {
+      response.on('data', (chunk) => {
+        responseBody += chunk;
+      });
+      response.on('end', () => {
+        const body = JSON.parse(responseBody);
+        checkReponse(response, body);
+        resolve(body);
+      });
     });
+  });
 }
 
 async function getMany(url) {
-    const MAX_PAGE = 10;
-    const bodies = [];
-    for (let page = 1; page <= MAX_PAGE; page += 1) {
-        const body = await get(url + '?per_page=100&page=' + page);
-        bodies.push(body);
-        if (body.length < 100) {
-            break;
-        }
+  const MAX_PAGE = 10;
+  const bodies = [];
+  for (let page = 1; page <= MAX_PAGE; page += 1) {
+    const body = await get(`${url}?per_page=100&page=${page}`);
+    bodies.push(body);
+    if (body.length < 100) {
+      break;
     }
-    return bodies.flat();
+  }
+  return bodies.flat();
 }
 
 // HTTP POST
 async function post(url, data) {
-    console.log('post ' + url);
-    const requestBody = JSON.stringify(data);
-    return new Promise((resolve) => {
-        let responseBody = '';
-        const options = {
-            headers: {
-                'Content-Type': 'application/json',
-                'Content-Length': Buffer.byteLength(requestBody),
-            },
-            auth: AUTH,
-            method: 'POST',
-        };
-        const request = https.request(url, options, response => {
-            response.on('data', chunk => responseBody += chunk);
-            response.on('end', () => {
-                const body = JSON.parse(responseBody);
-                checkReponse(response, body);
-                resolve(body);
-            });
-        });
-        request.write(requestBody);
-        request.end();
+  console.log(`post ${url}`);
+  const requestBody = JSON.stringify(data);
+  return new Promise((resolve) => {
+    let responseBody = '';
+    const options = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(requestBody)
+      },
+      auth: AUTH,
+      method: 'POST'
+    };
+    const request = https.request(url, options, (response) => {
+      response.on('data', (chunk) => (responseBody += chunk));
+      response.on('end', () => {
+        const body = JSON.parse(responseBody);
+        checkReponse(response, body);
+        resolve(body);
+      });
     });
+    request.write(requestBody);
+    request.end();
+  });
 }
 
 async function postFile(url, title, filename, media) {
-    console.log('post ' + url);
-    return new Promise((resolve) => {
-        let responseBody = '';
-        const form = new FormData();
-        form.append('title', title);
-        form.append('file', media, filename);
-        const data = form.getBuffer();
-        const options = {
-            headers: {
-                'Content-Type': form.getHeaders()['content-type'],
-                'Content-Length': data.length,
-            },
-            auth: AUTH,
-            method: 'POST',
-        };
-        const request = https.request(url, options, response => {
-            response.on('data', chunk => responseBody += chunk);
-            response.on('end', () => {
-                checkReponse(response, responseBody);
-                resolve([response, responseBody]);
-            });
-        });
-        request.write(data);
-        request.end();
+  console.log(`post ${url}`);
+  return new Promise((resolve) => {
+    let responseBody = '';
+    const form = new FormData();
+    form.append('title', title);
+    form.append('file', media, filename);
+    const data = form.getBuffer();
+    const options = {
+      headers: {
+        'Content-Type': form.getHeaders()['content-type'],
+        'Content-Length': data.length
+      },
+      auth: AUTH,
+      method: 'POST'
+    };
+    const request = https.request(url, options, (response) => {
+      response.on('data', (chunk) => (responseBody += chunk));
+      response.on('end', () => {
+        checkReponse(response, responseBody);
+        resolve([response, responseBody]);
+      });
     });
+    request.write(data);
+    request.end();
+  });
 }
 
 // https://developer.wordpress.org/rest-api/reference/posts/
 
 // fetch all stories which are currently available
 export async function postList() {
-    const body = await get(URL_POSTS + '?per_page=100&page=1');
-    return body;
+  const body = await get(`${URL_POSTS}?per_page=100&page=1`);
+  return body;
 }
 
 // title: string
 export async function postCreate(title) {
-    const body = await post(URL_POSTS, { title: title, status: 'publish' });
+  const body = await post(URL_POSTS, { title, status: 'publish' });
 }
 
 // id: integer
 // content: string
 // tags: array of integers
 export async function postUpdate(id, content, tags) {
-    const body = await post(URL_POSTS + '/' + id, { content: content, tags: tags, status: 'publish' });
+  const body = await post(`${URL_POSTS}/${id}`, { content, tags, status: 'publish' });
 }
 
 // https://developer.wordpress.org/rest-api/reference/tags/
 
 export async function tagList() {
-    const body = await getMany(URL_TAGS);
-    return body;
+  const body = await getMany(URL_TAGS);
+  return body;
 }
 
 // name: string
 export async function tagCreate(name) {
-    const body = await post(URL_TAGS, { name: name });
+  const body = await post(URL_TAGS, { name });
 }
 
 // https://developer.wordpress.org/rest-api/reference/categories/
 
 export async function categoryList() {
-    const body = await get(URL_CATEGORIES);
-    return body;
+  const body = await get(URL_CATEGORIES);
+  return body;
 }
 
 // https://developer.wordpress.org/rest-api/reference/media/
 
 export async function mediaList() {
-    const body = await getMany(URL_MEDIA);
-    return body;
+  const body = await getMany(URL_MEDIA);
+  return body;
 }
 
 export async function mediaCreate(title, filename, media) {
-    const body = await postFile(URL_MEDIA, title, filename, media);
+  const body = await postFile(URL_MEDIA, title, filename, media);
 }
